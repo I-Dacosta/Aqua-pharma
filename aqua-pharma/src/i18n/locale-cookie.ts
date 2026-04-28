3cc3a2d6cca7019c4dbcadd09d608bcbc08e734f
Ima Da costa <ima.dacosta@aquatiq.com>
2026-04-28 09:09:56 +0200

    cleaned structure

--- aqua-pharma/src/i18n/locale-cookie.ts
+++
@@ -1,7 +0,0 @@
-import { localeCookieName, type Locale } from "@/i18n/config";
-
-const ONE_YEAR_SECONDS = 60 * 60 * 24 * 365;
-
-export function writeLocaleCookie(nextLocale: Locale) {
-  document.cookie = `${localeCookieName}=${nextLocale}; path=/; max-age=${ONE_YEAR_SECONDS}; samesite=lax`;
-}
--- aqua-pharma/src/i18n/request.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers";
-import { defaultLocale, isLocale, type Locale, localeCookieName } from "@/i18n/config";
-
-export async function getRequestLocale(): Promise<Locale> {
-  const cookieStore = await cookies();
-  const cookieLocale = cookieStore.get(localeCookieName)?.value;
-
-  return isLocale(cookieLocale) ? cookieLocale : defaultLocale;
-}
